959e914b9d77e0526ef9ccf8592e781ed774e948
Josef21296 <jcasanovasv@hotmail.com>
2017-01-20 23:36:48 +0100

    Player prototype

---
+++ GhostFinders/Assets/ProjectAssets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Light flashLight = null;
+    public Camera flashLightFrustum = null;
+
+    Vector3 mouseScreenPos = Vector3.zero;
+
+    //----------------------------
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        mouseScreenPos = Input.mousePosition;
+        Debug.Log(mouseScreenPos);
+	}
+
+    void OnDrawGizmos()
+    {
+
+    }
+}
