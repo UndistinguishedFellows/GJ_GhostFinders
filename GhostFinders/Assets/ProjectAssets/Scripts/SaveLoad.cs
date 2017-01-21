9e4cd5a5f84fd48651a6638988e6776fcbb2f32b
dibu13 <dibu.13@gmail.com>
2017-01-21 16:45:50 +0100

    Save & Load Scores Function

---
+++ GhostFinders/Assets/ProjectAssets/Scripts/SaveLoad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class SaveLoad{
+    public static List<Score> savedScores = new List<Score>();
+
+    public static void Save()
+    {
+        savedScores.Add(Score.current);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/Scores.gd");
+        bf.Serialize(file, SaveLoad.savedScores);
+        file.Close();
+    }
+
+    public static void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/Scores.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/Scores.gd", FileMode.Open);
+            SaveLoad.savedScores = (List<Score>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+}
---
+++ GhostFinders/Assets/ProjectAssets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[System.Serializable]
+public class Score
+{
+
+    public static Score current;
+    public string name;
+    public int points;
+
+    public Score()
+    {
+        name = "";
+        points = 0;
+    }
+
+}
