c281be96a583d8b3025eca8ded99c66e13460c57
dibu13 <dibu.13@gmail.com>
2017-01-22 02:19:26 +0100

    Main menu

---
+++ GhostFinders/Assets/ProjectAssets/Scripts/ExitButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour {
+
+    bool exitGame = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(exitGame) Application.Quit();
+
+    }
+
+    public void onClick()
+    {
+        Debug.Log("EXIT");
+        Application.Quit();
+        exitGame = false;
+    }
+
+}
