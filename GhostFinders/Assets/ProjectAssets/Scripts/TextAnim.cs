b0ecefdd1d8d662c26741cb8d09d7cc8bfaa6ec4
dibu13 <dibu.13@gmail.com>
2017-01-22 06:02:51 +0100

    Tutorial started

---
+++ GhostFinders/Assets/ProjectAssets/Scripts/TextAnim.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextAnim : MonoBehaviour {
+
+    public Text txt;
+    public string texto;
+    public GameObject cont;
+    public GameObject ant;
+    public GameObject sig;
+    bool click = false;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(dictado(texto));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!cont.gameObject.activeSelf && click == true) cont.SetActive(true);
+
+        if (Input.GetMouseButtonDown(0) && cont.gameObject.activeSelf)
+        {
+            ant.SetActive(false);
+            cont.SetActive(false);
+            sig.SetActive(true);
+        }
+    }
+
+    IEnumerator dictado(string frase)
+    {
+        int letra = 0;
+        txt.text = "";
+        while (letra < frase.Length)
+        {
+            txt.text += frase[letra];
+            letra += 1;
+            yield return new WaitForSeconds(0.02f);
+        }
+        click = true;
+    }
+
+}
