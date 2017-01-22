1aa5e5449e8a3033944a6229a248f2e453c4a1d5
CapitanLiteral <anaraguen@gmail.com>
2017-01-22 05:32:14 +0100

    Flashlight UI

---
+++ GhostFinders/Assets/ProjectAssets/Scripts/FlashlighManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlighManager : MonoBehaviour {
+    [SerializeField]
+    GameObject[] lanterns;
+    [SerializeField]
+    PlayerController pController;
+    Scripts.colors current_color;
+    Scripts.colors last_color = Scripts.colors.red;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (last_color != pController.fLColor)
+        {
+            lanterns[(int)last_color - 1].SetActive(false);
+            last_color = pController.fLColor;
+            lanterns[(int)last_color - 1].SetActive(true);
+        }
+	}
+}
