e42f1c238a036d15ad83b1d30b77aa3f4b048534
CapitanLiteral <anaraguen@gmail.com>
2017-01-22 10:01:57 +0100

    Splash Scene

---
+++ GhostFinders/Assets/SplashGlobalGameJamTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashGlobalGameJamTimer : MonoBehaviour {
+    float time = 2f;
+    float elapsedTime = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= time)
+        {
+            SceneManager.LoadScene("Level_1");
+        }
+	}
+}
